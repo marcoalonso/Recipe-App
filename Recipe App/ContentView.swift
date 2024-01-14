81f1fca28cc9e1e56ef43e38442c4843c78d1cc0
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2024-01-14 12:49:14 -0600

    Getting Started

---
+++ Recipe App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recipe App
+//
+//  Created by Marco Alonso Rodriguez on 14/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Marco Alonso Rodriguez on 14/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
