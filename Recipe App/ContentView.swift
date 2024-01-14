9c94ea88889c1cda7330bb35fa1e8b96dd9eb455
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2024-01-14 12:55:41 -0600

    Get Started Screen

--- Recipe App/ContentView.swift
+++ Recipe App/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .center) {
+            Image("foods")
+                .resizable()
+                .frame(height: 400)
+            
+            VStack(spacing: 30.0) {
+                Text("Start Cooking")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                
+                Text("Let's join our community \n to cook better food!")
+                    .font(.title3)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.gray)
+            }
+                
+            
+            Spacer()
+            
+            Button(action: {
+                
+            }, label: {
+                Text("Get Started")
+                    .font(.subheadline)
+                    .frame(height: 48)
+                    .frame(maxWidth: .infinity)
+                    .foregroundColor(.white)
+                    .padding(.horizontal)
+                    .background(Color(.green))
+                    .cornerRadius(24)
+                    .padding(.horizontal, 24)
+            })
+            //.padding(.top, 40)
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
