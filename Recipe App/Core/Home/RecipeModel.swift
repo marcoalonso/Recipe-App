17ba5866e575e928e25e3c685b598c6719cd4745
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2024-01-17 14:33:16 -0600

    Mock Data added

---
+++ Recipe App/Core/Home/RecipeModel.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeModel.swift
+//  Recipe App
+//
+//  Created by Marco Alonso Rodriguez on 17/01/24.
+//
+
+import Foundation
+
+struct RecipeModel {
+    let owner: String
+    let dishPicture: String
+    let ownerPicture: String
+    let isFavourite: Bool
+    let dishName: String
+    let descriptionPreparation: String
+    let minutesPreparation: Int
+}
+
+
---
+++ Recipe App/Core/Home/View/RecipeCellView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeCellView.swift
+//  Recipe App
+//
+//  Created by Marco Alonso Rodriguez on 17/01/24.
+//
+
+import SwiftUI
+
+struct RecipeCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeCellView()
+}
